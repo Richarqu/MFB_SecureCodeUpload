1513708fea9d2dc3127c604adf0d4e07b3679632
Richarqu <richarque@gmail.com>
2020-11-24 01:09:10 +0100

    added account inquire for voguepay

---
+++ ConsoleJobs/VogueAcctInqResp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleJobs
+{
+    public class VogueAcctInqResp
+    {
+        public string code { get; set; }
+        public string message { get; set; }
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public string customerID { get; set; }
+        public string accountType { get; set; }
+        public string accountName { get; set; }
+        public string emailAddress { get; set; }
+        public string phoneNumber { get; set; }
+        public string homeAddress { get; set; }
+    }
+
+}
